e0da22c297e05a2cdfa0ec839b621c2eb6f7ff0f
AugustoMagalhaes <augustommg@gmail.com>
2022-01-25 18:15:04 -0300

    R01: Implementando a funçao 'getSpeciesByIds' que retorna uma array contendo apenas as especies de 'data' cujos ids fora passados como argumentos.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+  const dataSpecies = data.species;
+  const selected = dataSpecies.filter((element) => (ids.includes(element.id) ? element : false));
+  return selected;
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
