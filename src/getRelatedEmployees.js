4f26e60201472d196c06bb1c702716ba729ef21c
AugustoMagalhaes <augustommg@gmail.com>
2022-01-26 18:49:53 -0300

    R04: Implementando a funçao 'getRelatedEmployees' que retorna, caso exista, um array com os nomes de funcionarios administrados por um gerente.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
+const [stephanieId, olaId, burlId] = ['9e7d4524-363c-416a-8759-8aa7e50c0992',
+  'fdb2543b-5662-46a7-badc-93d960fdc0a8',
+  '0e7b460e-acf4-4e17-bcb3-ee472265db83'];
+
 function isManager(id) {
   // seu código aqui
+  return [stephanieId, olaId, burlId].includes(id);
 }
+
+const relatedEmployees = (list, id) => list.reduce((related, employee) => {
+  if (employee.managers.includes(id)) {
+    related.push(`${employee.firstName} ${employee.lastName}`);
+  }
+  return related;
+}, []);
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  const employeeList = data.employees;
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return relatedEmployees(employeeList, managerId);
 }
 
 module.exports = { isManager, getRelatedEmployees };
