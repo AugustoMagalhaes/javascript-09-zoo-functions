c52cd5db255d8538c81051f0b755796d37ac4ff1
AugustoMagalhaes <augustommg@gmail.com>
2022-01-26 02:41:17 -0300

    R02: Implementando a funçao 'getAnimalsOlderThan' de modo que teste se o animal e a idade passados como parametros retornam um valor booleano adequado.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const getAnimal = (element) => (element.name === animal ? element : false);
+  const findAnimal = data.species.find(getAnimal);
+  const checkAge = findAnimal.residents.every((specimen) => specimen.age > age);
+  return checkAge;
 }
 
 module.exports = getAnimalsOlderThan;
