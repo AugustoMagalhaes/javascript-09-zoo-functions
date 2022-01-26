d42ac87456a65d1a721e2af11ff3c3b4960cb68d
AugustoMagalhaes <augustommg@gmail.com>
2022-01-26 04:35:32 -0300

    R03: Implementando a funçao 'getEmployeeByName' que retorna o objeto de um determinado funcionario ao passar seu primeiro nome ou ultimo nome.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,13 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return {};
+  const allEmployees = data.employees;
+  /* Após muitas tentativas de reduzir o tamanho da linha seguinte, cheguei a esta ideia aproveitando a lógica de 'destructuring',
+  embora de maneira 'invertida' */
+  const nameCheck = (employee) => ([employee.firstName, employee.lastName].includes(employeeName));
+  const selectedEmployee = allEmployees.find(nameCheck);
+  return selectedEmployee;
 }
 
 module.exports = getEmployeeByName;
