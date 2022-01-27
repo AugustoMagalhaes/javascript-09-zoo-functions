549c957bbe69e112b7d23c86449b13c47f734eee
AugustoMagalhaes <augustommg@gmail.com>
2022-01-27 17:21:36 -0300

    R06: Implementando a funçao 'calculateEntry' que, ao receber uma array de visitantes, retorna como 'output' final o valor da soma de todos os ingressos.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,33 @@
 const data = require('../data/zoo_data');
 
+const entrantsByAge = (list) => {
+  const fakeObj = { child: 0, adult: 0, senior: 0 };
+  const reducer = list.reduce((acc, curr) => {
+    let key = '';
+    key = (curr.age >= 50) ? 'senior' : 'child';
+    if (curr.age >= 18 && curr.age < 50) {
+      key = 'adult';
+    }
+    fakeObj[key] += 1;
+    return fakeObj;
+  }, fakeObj);
+  return reducer;
+};
+
 function countEntrants(entrants) {
   // seu código aqui
+  return entrantsByAge(entrants);
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  const dataPrices = data.prices;
+  const getObj = countEntrants(entrants);
+  const { child, adult, senior } = data.prices;
+  return child * getObj.child + adult * getObj.adult + senior * getObj.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
