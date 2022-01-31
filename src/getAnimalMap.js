1b67e2f2b3f18923f92562227dd0878aac7f2c64
AugustoMagalhaes <augustommg@gmail.com>
2022-01-31 13:52:53 -0300

    R07.1: implementando a funçao 'getAnimalMap' com as funcionalidades de retornar um objeto divididos por pontos cardeais, cada um sendo uma array com os animais respectivamente alojados; o mesmo tambem ocorre quando nao se especifica 'includeNames' no argumento.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+const getLocation = (list) => {
+  const obj = {};
+  ['NE', 'NW', 'SE', 'SW'].forEach((locationX) => {
+    obj[locationX] = [];
+    list.forEach((element) => {
+      if (element.location === locationX) {
+        obj[locationX].push(element.name);
+      }
+    });
+  });
+  return obj;
+};
+
+console.log(getLocation(data.species));
 function getAnimalMap(options) {
   // seu código aqui
+  const dataSpecies = data.species;
+  if (!options || !options.includeNames) {
+    return getLocation(dataSpecies);
+  }
 }
 
 module.exports = getAnimalMap;
