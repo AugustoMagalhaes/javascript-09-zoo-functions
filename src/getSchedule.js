dadf4c1fc762405fc8026f295b1c7fe5cc6db057
AugustoMagalhaes <augustommg@gmail.com>
2022-02-01 02:06:01 -0300

    R08.1: Implementando as funçoes 'exhibitedAnimals' e 'daySchedule' que permitem a principal 'getSchedule' retornar o array indicado quando nao sao passados argumentos para ela.

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
+const dataHours = data.hours;
+const dataSpecies = data.species;
+
+const exhibitedAnimals = (day) => {
+  const filteredAnimals = dataSpecies.filter((animal) => animal.availability.includes(day));
+  const animalsOfDay = filteredAnimals.map((animal) => animal.name);
+  return animalsOfDay;
+};
+
+const daySchedule = (...dayParam) => {
+  const expected = {};
+  const dayNames = Object.keys(dataHours);
+  const dayHours = Object.values(dataHours);
+  dayNames.forEach((day, index, array) => {
+    const openingHours = dayHours[index];
+    const obj = {
+      officeHour: (openingHours.open + openingHours.close !== 0)
+        ? `Open from ${openingHours.open}am until ${openingHours.close}pm`
+        : 'CLOSED',
+      exhibition: (exhibitedAnimals(day).length > 0) ? exhibitedAnimals(day)
+        : 'The zoo will be closed!',
+    };
+    expected[day] = obj;
+  });
+  return expected;
+};
+
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  if (!scheduleTarget) {
+    return daySchedule();
+  }
 }
+
+console.log(getSchedule());
 
 module.exports = getSchedule;
