801e39575237caa087160729d1d126bc82c6c3bd
AugustoMagalhaes <augustommg@gmail.com>
2022-02-02 01:06:42 -0300

    R10.1: Implementando a funçao 'getEmployeesCoverage' com a exceçao de quando o argumento nao e' encontrado.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+const allEmployees = data.employees;
+const allSpecies = data.species;
+
+const getAnimalName = (coveredId) => allSpecies.find((animal) => animal.id === coveredId).name;
+
+const getAnimalLocation = (coveredId) => allSpecies.find((animal) =>
+  animal.id === coveredId).location;
+
+console.log(getAnimalLocation('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
+
+const findEmployeeCoverage = (employeeInfo) => {
+  const info = Object.values(employeeInfo);
+  const findEmployee = allEmployees.find((employee) => Object.values(employee).includes(...info));
+  return {
+    id: `${findEmployee.id}`,
+    fullName: `${findEmployee.firstName} ${findEmployee.lastName}`,
+    species: findEmployee.responsibleFor.map(getAnimalName),
+    locations: findEmployee.responsibleFor.map(getAnimalLocation),
+  };
+};
+
+function getEmployeesCoverage(employeeInfo) {
   // seu código aqui
+  if (!employeeInfo) {
+    const allEmployeesCoverage = allEmployees.map(findEmployeeCoverage);
+    return allEmployeesCoverage;
+  }
+  return findEmployeeCoverage(employeeInfo);
 }
 
+console.log(getEmployeesCoverage());
 module.exports = getEmployeesCoverage;
